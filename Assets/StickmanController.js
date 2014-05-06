40393b70bcaca5bdc5160935d936f24d20aeabff
Gerry Said <gerrysaid@gmail.com>
2014-05-06 17:39:49 +0200

    Lesson 6th May 2013

--- Assets/StickmanController.js
+++ Assets/StickmanController.js
@@ -8,6 +8,10 @@ function Start () {
 }
 
 function Update () {
+	if (transform.position.y > -0.6543808)
+	{
+		transform.Translate(Vector3.down * 5 * Time.deltaTime);
+	}
 
 	//this is the code that happens when I press the left arrow key
 	if (Input.GetKey(KeyCode.LeftArrow)) {
@@ -17,6 +21,12 @@ function Update () {
 	//this is the code that happens when I press the right arrow key
 	if (Input.GetKey(KeyCode.RightArrow)) {
 		transform.Translate(Vector3.right * stickmanSpeed * Time.deltaTime);
+	}
+	
+	if (Input.GetKeyDown(KeyCode.Space))
+	{
+		transform.Translate(Vector3.up * 100 * Time.deltaTime);
+		
 	}
 
 }
