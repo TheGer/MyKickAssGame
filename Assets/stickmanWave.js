a7be73d49edbb6c01b85c276add9a06efb815864
Gerry Said <gerrysaid@gmail.com>
2014-05-06 20:13:21 +0200

    Lesson 6th May 2014
    Lesson

---
+++ Assets/stickmanWave.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var anim:Animator;
+
+function Start () {
+	
+	//so I can change parameters
+	anim = GetComponent(Animator);
+}
+
+function Update () {
+	
+	anim.SetBool("startWaving",false);
+	
+	if (Input.GetKey(KeyCode.W))
+	{
+		anim.SetBool("startWaving",true);
+	}
+
+}
