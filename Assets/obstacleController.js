1df1a20958c51e23cf5f4167d524ca08f6b44952
Gerry Said <gerrysaid@gmail.com>
2014-05-13 20:12:50 +0200

    Lesson 13th May 2014
    Lesson

---
+++ Assets/obstacleController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var speed:int;
+
+function Start () {
+
+}
+
+function Update () {
+	//moving cube to the left at the speed which we set in unity
+	transform.Translate(Vector3.left * speed * Time.deltaTime); 
+}
+
+//when the cube goes out of the screen
+function OnBecameInvisible()
+{
+	//destroy the cube that THIS script is attached to
+	Destroy(this.gameObject);
+}
---
+++ Assets/obstacleGenerator.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+//slot for the prefab
+var obstacle:Rigidbody;
+
+//a variable to determine the time gap
+var timeGap:float;
+
+
+function Start () {
+
+	yield WaitForSeconds(1.0);
+	yield createObstacle();
+	//InvokeRepeating("createObstacle",0.0f,0.25f);
+}
+
+
+function createObstacle()
+{
+	//keep running this while the game is running
+	while (true)
+	{
+		//create a cube
+		Instantiate(obstacle,transform.position,transform.rotation);
+		//generate a random time gap
+		timeGap = Random.Range(0.5f,2.0f);
+		//waits for a random time between 0.5 and 2.0
+		yield WaitForSeconds(timeGap);
+		//output the generated time gap in the console
+		Debug.Log(timeGap);
+	}
+	
+}
+
+function Update () {
+
+}
