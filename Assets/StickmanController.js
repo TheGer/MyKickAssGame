8c9d0b6ac5f7d05f905cb4fae67130d244ee4b8e
Gerry Said <gerrysaid@gmail.com>
2014-05-21 09:16:27 +0200

    Addition of different obstacle as well as implementation of smaller cube and shorter jump movement.

--- Assets/StickmanController.js
+++ Assets/StickmanController.js
@@ -2,12 +2,102 @@
 //stickmanSpeed is a number that sets the speed of the stickman
 var stickmanSpeed:int;
 
+var stickmanLives:int=3;
+
+var stickmanPicture:Texture;
+
+//can tell if the guy is on the ground or not
+var onGround:boolean=true;
+//http://L047-T:89
+function OnTriggerExit(theotherbox:Collider)
+{
+	if (theotherbox.tag == "ground")
+	{
+		onGround = false;
+	}
+}
+
+function OnTriggerStay(theotherbox:Collider)
+{
+	if (theotherbox.tag == "ground")
+		{
+			onGround=true;
+		}
+		
+}
+
+//function that happens when the player is hit by something
+function OnTriggerEnter(theotherbox:Collider){
+	
+	if (theotherbox.tag == "ground")
+	{
+		//what happens when the object hits the ground
+		onGround = true;
+	}
+
+	//only trigger when hit by obstacle
+	if (theotherbox.tag == "obstacle") {
+		//this is the code that happens when i get hit
+		stickmanLives--;
+		//destroy the obstacle
+		Destroy(theotherbox.gameObject);
+	}
+}
+
+
+
+
+
+//GUI function
+function OnGUI() {
+	//create a label in the top left of the screen
+	//0,0 xy coordinate of the top left
+	//150,20 width and height of the bounding box
+	//for the text
+	GUI.Label(Rect(0,0,150,20),"My name is GERARD");
+	
+	//width of the screen
+	GUI.Label(Rect(0,20,150,20),"Width: "+Screen.width);
+	
+	//height of the screen
+	GUI.Label(Rect(0,40,150,20),"Height: "+Screen.height);
+	
+	//using the resolution, going to the other side of the screen
+	GUI.Label(Rect(Screen.width-150,0,150,20),"Other side of screen");
+	
+	//this is a for loop
+	for(var counter=0;counter<stickmanLives;counter++)
+	{
+		//draw the picture
+		var spacing:int;
+	
+		spacing = (25 * counter) + 2;  
+	
+		GUI.DrawTexture(Rect(spacing,60,25,25),stickmanPicture,ScaleMode.ScaleToFit,true);
+	
+	}
+	
+	
+}
+
 
 function Start () {
 
 }
 
 function Update () {
+	//this will write stickman on ground true/false in 
+	//the console:
+	Debug.Log("Stickman on Ground: "+onGround);
+	
+	//if I run out of lives
+	if (stickmanLives <= 0)
+	{
+		//Game Over - reset the game
+		Application.LoadLevel(0);
+	}
+	
+	//fake gravity
 	if (transform.position.y > -0.6543808)
 	{
 		transform.Translate(Vector3.down * 5 * Time.deltaTime);
@@ -25,8 +115,12 @@ function Update () {
 	
 	if (Input.GetKeyDown(KeyCode.Space))
 	{
-		transform.Translate(Vector3.up * 100 * Time.deltaTime);
-		
+		//code that happens when I press space
+		if (onGround == true)
+		{
+			//jump
+			transform.Translate(Vector3.up * 5 * Time.deltaTime);
+		}
 	}
 
 }
--- Assets/obstacleGenerator.js
+++ Assets/obstacleGenerator.js
@@ -1,10 +1,12 @@
 ﻿#pragma strict
 //slot for the prefab
 var obstacle:Rigidbody;
+var obstacle2:Rigidbody;
 
 //a variable to determine the time gap
 var timeGap:float;
 
+var obstacleChoice:int=0;
 
 function Start () {
 
@@ -19,8 +21,22 @@ function createObstacle()
 	//keep running this while the game is running
 	while (true)
 	{
-		//create a cube
+		obstacleChoice = Random.Range(0,2);
+		//random value between 0 and 1
+		//output the value of obstacle choice
+		Debug.Log(obstacleChoice);
+		//if the value is 1 do the red cube
+		if (obstacleChoice == 0)
+		{
+		//create a  red cube
 		Instantiate(obstacle,transform.position,transform.rotation);
+		}
+		//if the value is 2 do the green cube
+		if (obstacleChoice == 1)
+		{
+		Instantiate(obstacle2,transform.position,transform.rotation);
+		}
+		
 		//generate a random time gap
 		timeGap = Random.Range(0.5f,2.0f);
 		//waits for a random time between 0.5 and 2.0
