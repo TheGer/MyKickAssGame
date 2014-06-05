e01b1a2c2cac714cf3df45edf81d22165a30a35e
Gerry Said <gerrysaid@gmail.com>
2014-06-05 19:52:38 +0200

    Lesson 6th June
    Attached

---
+++ Assets/InputScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var textToUpdate="Text to update";
+
+
+function Start () {
+	
+}
+
+function Update () {
+	
+	
+}
+
+
+function OnGUI()
+{
+	//text field for input
+	textToUpdate = GUI.TextField(Rect(0,Screen.height-50,100,50),textToUpdate);
+	
+	//button to click to confirm input
+	if (GUI.Button(Rect(0,Screen.height-100,50,50),"Set Name"))
+	{
+		//output the name input in the console	
+		Debug.Log("The name is now set to: "+textToUpdate);
+		
+	}
+}
---
+++ Assets/MySpriteController.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var movementspeed:int;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	GetComponent(Animator).SetBool("moving",false);
+	
+	if (Input.GetKey(KeyCode.LeftArrow))
+	{
+		//flip
+		transform.localScale.x = -1;
+		//left arrow
+		transform.Translate(Vector3.left * movementspeed * Time.deltaTime);
+		//set the animation on
+		GetComponent(Animator).SetBool("moving",true);
+		
+	}
+	
+	
+	if (Input.GetKey(KeyCode.RightArrow))
+	{
+		//flip
+		transform.localScale.x = 1;
+		//left arrow
+		transform.Translate(Vector3.right * movementspeed * Time.deltaTime);
+		//set the animation on
+		GetComponent(Animator).SetBool("moving",true);
+		
+	}
+	
+	
+}
